e766d985ec1c245140e012fbee4dd04f41a864b7
Paul Sutcliffe <frpaulas@gmail.com>
2019-05-30 07:47:25 -0400

    general improvements

--- services/MorningPrayer.elm
+++ services/MorningPrayer.elm
@@ -44,6 +44,120 @@ source =
     redeemer. 
     --
 
+| Seasonal
+    ofType: opening_sentence
+    --
+    tag: advent
+    label: Advent
+    ref: isaiah 40:3
+    text: In the wilderness prepare the way of the Lord; make straight in
+    the desert a highway for our God. 
+    --
+    tag: christmas
+    label: Christmas
+    ref: luke 2:10-11
+    text: Fear not, for behold, I bring you good news of great joy that will
+    be for all the people. For unto you is born this day in the city of
+    David a Savior, who is Christ the Lord.
+    --
+    tag: epiphany
+    label: Epiphany
+    ref: malachi 1:11
+    text: From the rising of the sun to its setting my name will be great
+    among the nations, and in every place incense will be offered to
+    my name, and a pure offering. For my name will be great among
+    the nations, says the Lord of hosts.
+    --
+    tag: lent
+    label: lent and other penitential occasions
+    ref: matthew 3:2
+    text: Repent, for the kingdom of heaven is at hand.
+    --
+    tag: lent
+    label: BLANK
+    ref: psalm 51:9
+    text: Turn your face from my sins,
+    and blot out all my misdeeds.
+    --
+    tag: lent
+    label: BLANK
+    ref: mark 8:34
+    text: If anyone would come after me, let him deny himself and take
+    up his cross and follow me.
+    --
+    tag: holy_week
+    label: Holy Week
+    ref: lamentations 1:12
+    text: Is it nothing to you, all you who pass by? Look and see
+    if there is any sorrow like my sorrow, which was brought
+    upon me, which the Lord inflicted on the day of his
+    fierce anger.
+    --
+    tag: easter
+    label: Easter
+    ref: colossians 3:1
+    text: If then you have been raised with Christ, seek the things
+    that are above, where Christ is, seated at the right hand
+    of God.
+    --
+    tag: ascension
+    label: Ascension
+    ref: hebrews 4:14, 16
+    text: Since then we have a great high priest who has passed through
+    the heavens, Jesus, the Son of God, let us hold fast our
+    confession. Let us then with confidence draw near to the throne
+    of grace, that we may receive mercy and find grace to help in
+    time of need.
+    --
+    tag: pentecost
+    label: Pentecost
+    ref: acts 1:8
+    text: You will receive power when the Holy Spirit has come upon
+    you, and you will be my witnesses in Jerusalem and in all Judea
+    and Samaria, and to the end of the earth.
+    --
+    tag: trinity
+    label: Trinity Sunday
+    ref: revelation 4:8
+    text: Holy, holy, holy, is the Lord God Almighty, who was and is and
+    is to come!
+    --
+    tag: thanksgiving
+    label: days of thanksgiving
+    ref: proverbs 3:9-10
+    text: Honor the Lord with your wealth and with the firstfruits of all
+    your produce; then your barns will be filled with plenty, and your
+    vats will be bursting with wine.
+    --
+    tag: anytime
+    label: at any time
+    ref: habakkuk 2:20
+    text: The Lord is in his holy temple; let all the earth keep silence
+    before him.
+    --
+    tag: anytime
+    label: BLANK
+    ref: psalm 43:3
+    text: O send out your light and your truth, that they may lead me, and
+    bring me to your holy hill, and to your dwelling.
+    --
+    tag: anytime
+    label: BLANK
+    ref: isaiah 57:15
+    text: Thus says the One who is high and lifted up, who inhabits
+    eternity, whose name is Holy: “I dwell in the high and holy
+    place, and also with him who is of a contrite and lowly spirit,
+    to revive the spirit of the lowly, and to revive the heart of
+    the contrite.”
+    --
+    tag: anytime
+    label: BLANK
+    ref: john 4:23
+    text: The hour is coming, and is now here, when the true worshipers
+    will worship the Father in spirit and truth, for the Father is
+    seeking such people to worship him. 
+    --
+
 | Section
     confession of sin
 
