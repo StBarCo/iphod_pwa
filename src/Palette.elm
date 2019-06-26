d200d9534d24641736a983bc72bed3dd3890d131
Paul Sutcliffe <frpaulas@gmail.com>
2019-06-25 21:31:20 -0400

    Add "pi" class

--- src/Palette.elm
+++ src/Palette.elm
@@ -96,11 +96,11 @@ class name =
         "q" ->
             []
         "q2" ->
-            [ paddingEach { top = 0, right = 0, bottom = 0, left = 10}]
+            [ paddingEach {edges | left = 10} ]
+        "pi" ->
+            [ paddingEach {edges | left = 10}
+            ]
         _ ->
-            -- let
-            --     _ = Debug.log "UNRECOGNIZED CLASS NAME:" name
-            -- in
             []
 
 antiphon : Model -> List (Attribute msg)
